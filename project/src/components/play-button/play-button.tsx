a26118764e4a6929e2206492e2bc2493bfddd4e5
Iakimov Sergey <serg.n.y@yandex.ru>
2023-01-15 05:12:14 +0500

    add player

--- project/src/components/play-button/play-button.tsx
+++ project/src/components/play-button/play-button.tsx
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom';
 
 type PlayButtonProps = {
   filmId: number;
@@ -6,12 +7,12 @@ type PlayButtonProps = {
 
 function PlayButton({filmId}: PlayButtonProps): JSX.Element {
   return (
-    <button className="btn btn--play film-card__button" type="button">
+    <Link to={`/player/${filmId}`} className="btn btn--play film-card__button">
       <svg viewBox="0 0 19 19" width="19" height="19">
         <use xlinkHref="#play-s"></use>
       </svg>
       <span>Play</span>
-    </button>);
+    </Link>);
 }
 
 export default PlayButton;
